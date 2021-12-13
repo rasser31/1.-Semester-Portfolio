47c89d726f63722ed534a917c4aec75c03dfa4c7
Rasmus Pedersen <rasser31@hotmail.com>
2021-12-13 13:32:13 +0100

    Tilføjelse af modal bokse baggrunde, samt de sidste knapper.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
+// -------------------
 // Javascript dokument
+// -------------------
 
+// -----------------------------------------------------------------------------------------
 /* For at kunne lave mine modal bokse, har jeg benyttet linket nedenunder */
 /* https://stackoverflow.com/questions/40645032/creating-multiple-modals-on-a-single-page */
+// -----------------------------------------------------------------------------------------
 
-// Get the button that opens the modal
+// Her laver jeg en variabel jeg kan benytte med min knap
 var btn = document.querySelectorAll("button.modal-button");
 
-// All page modals
+// Her laver jeg en variabel som danner en sammenhæng mellem alle mine modal bokse
 var modals = document.querySelectorAll('.modal');
 
-// Get the <span> element that closes the modal
+// Her laver jeg en variabel som benytter classet "close" (Se <span> i HTML, eller "close" classet i CSS)
 var spans = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal
@@ -26,8 +30,8 @@ for (var i = 0; i < spans.length; i++) {
     spans[i].onclick = function() {
         for (var index in modals) {
         if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+        }
     }
-}
 }
 
 // When the user clicks anywhere outside of the modal, close it
